8013035bfde6fe9f570f7d32368af180149ce17a
Manoel <manoel_am.neto@hotmail.com>
2025-04-11 16:43:55 -0300

    remocao arquivos java

--- ChatCI/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        GerenciadorUsuarios gerenciador = new GerenciadorUsuarios();
-        InterfaceUsuario interfaceUsuario = new InterfaceUsuario(gerenciador);
-        interfaceUsuario.exibirMenu();
-    }
-}
